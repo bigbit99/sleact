f5b43bbe920eb33998abafaa80a6b0ddf261ade8
bigbit99 <bigbit831@gmail.com>
2023-03-17 19:41:49 +0900

    디엠, 채널 리스트 컴포넌트 추가

--- setting/front/layouts/Workspace/index.tsx
+++ setting/front/layouts/Workspace/index.tsx
@@ -31,6 +31,8 @@ import { toast } from 'react-toastify';
 
 import InviteWorkspaceModal from '@components/InviteWorkspaceModal';
 import InviteChannelModal from '@components/InviteChannelModal';
+import DMList from '@components/DMList';
+import ChannelList from '@components/ChannelList';
 
 const Channel = loadable(() => import('@pages/Channel'));
 const DirectMessage = loadable(() => import('@pages/DirectMessage'));
@@ -180,9 +182,11 @@ const Workspace: VFC = () => {
                 <button onClick={onLogout}>로그아웃</button>
               </WorkspaceModal>
             </Menu>
-            {channelData?.map((v) => (
+            <ChannelList />
+            <DMList />
+            {/* {channelData?.map((v) => (
               <div>{v?.name}</div>
-            ))}
+            ))} */}
           </MenuScroll>
         </Channels>
         <Chats>
