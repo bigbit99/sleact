3ae8bf7859b0c66e9a862a3aed7bde662910ca9b
bigbit99 <bigbit831@gmail.com>
2023-03-18 14:42:41 +0900

    autosize 적용, textarea에 useRef적용

--- setting/front/components/ChatBox/index.tsx
+++ setting/front/components/ChatBox/index.tsx
@@ -1,19 +1,48 @@
 import { ChatArea, Form, MentionsTextarea, SendButton, Toolbox } from '@components/ChatBox/styles';
-import React, { useCallback, VFC } from 'react';
+import React, { useCallback, useEffect, useRef, VFC } from 'react';
+import autosize from 'autosize';
 
 interface Props {
   chat: string;
+  onSubmitForm: (e: any) => void;
+  onChangeChat: (e: any) => void;
+  placeholder?: string;
 }
 
-const ChatBox: VFC<Props> = ({ chat }) => {
-  const onSubmitForm = useCallback(() => {}, []);
+const ChatBox: VFC<Props> = ({ chat, onSubmitForm, onChangeChat, placeholder }) => {
+  // const onSubmitForm = useCallback(() => {}, []); //챗박스는 dm이랑 채널메시지 등 재사용 되어야하는 동작이라,
+  //컴포넌트에서 구체적으로 작동하지 않고, props로 올려줄거임
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null); //태그에 직접 접근하고 싶을 때 ref를 쓴다!
+  useEffect(() => {
+    if (textareaRef.current) {
+      autosize(textareaRef.current);
+    }
+  }, []);
+
+  const onKeydownChat = useCallback(
+    (e) => {
+      if (e.key === 'Enter') {
+        if (!e.shiftKey) {
+          e.preventDefault();
+          onSubmitForm(e);
+        }
+      }
+    },
+    [onSubmitForm],
+  );
 
   return (
     <ChatArea>
       <Form onSubmit={onSubmitForm}>
-        <MentionsTextarea>
-          <textarea></textarea>
-        </MentionsTextarea>
+        <MentionsTextarea
+          id="editor-chat"
+          value={chat}
+          onChange={onChangeChat}
+          onKeyDown={onKeydownChat}
+          placeholder={placeholder}
+          ref={textareaRef}
+        />
         <Toolbox>
           <SendButton
             className={
--- setting/front/pages/DirectMessage/index.tsx
+++ setting/front/pages/DirectMessage/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import useSWR from 'swr';
 import fetcher from '@utils/fetcher';
 import { Container, Header } from '@pages/DirectMessage/styles';
 import gravatar from 'gravatar';
 import { useParams } from 'react-router';
 import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSWR(`/api/users`, fetcher);
+
+  const [chat, onChangeChat] = useInput('');
+
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    console.log('submit');
+  }, []);
 
   if (!userData || !myData) {
     return null;
@@ -21,8 +30,8 @@ const DirectMessage = () => {
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      {/* <ChatList />*/}
-      <ChatBox chat="" />
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
 };
